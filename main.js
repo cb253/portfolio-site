e3a00554dd7f0c9cdbc70d5f63ef028584d31b89
Christian Belarde <cjbelarde253@gmail.com>
2024-02-29 21:55:08 -0800

    Updates to header

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(){
-    export const name = "main";
-    const main = document.querySelector('#main');
-    const div = document.creatElement("div");
-    div.innerHTML = "Hello World";
-    main.appendChild(div);
-});
+window.onload = function() {
+  const main = document.querySelector('#main');
+  const headerStarters = ["name:", "role:", "location:", "experience:", "resume:"];
+  const headerEnders = ["Christian Belarde", "Web Developer", "Seattle, WA", "Over 2 years", "Here"];  
+  main.innerHTML = `
+  <div>${headerStarters[0]}${headerEnders[0]}</div>
+  <div>${headerStarters[1]}${headerEnders[1]}</div>
+  <div>${headerStarters[2]}${headerEnders[2]}</div>
+  <div>${headerStarters[3]}${headerEnders[3]}</div>
+  <div>${headerStarters[4]}${headerEnders[4]}</div>
+  <div>${headerStarters[5]}${headerEnders[5]}</div>
+  `;
+};
+console.log(headerStarters);
