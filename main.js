ed97bb943e9e73b01dc8d7e35f788a68d2eb5e68
Christian Belarde <cjbelarde253@gmail.com>
2024-03-03 13:29:28 -0800

    Add initialzing for header

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 window.onload = function() {
   const main = document.querySelector('#main');
-  const headerStarters = ["name:", "role:", "location:", "experience:", "resume:"];
-  const headerEnders = ["Christian Belarde", "Web Developer", "Seattle, WA", "Over 2 years", "Here"];  
-  main.innerHTML = `
-  <div>${headerStarters[0]}${headerEnders[0]}</div>
-  <div>${headerStarters[1]}${headerEnders[1]}</div>
-  <div>${headerStarters[2]}${headerEnders[2]}</div>
-  <div>${headerStarters[3]}${headerEnders[3]}</div>
-  <div>${headerStarters[4]}${headerEnders[4]}</div>
-  <div>${headerStarters[5]}${headerEnders[5]}</div>
-  `;
-};
-console.log(headerStarters);
+  const headerContainer = document.createElement('div');
+  main.appendChild(headerContainer);
+
+  class headerInfo {
+    constructor (firstPart, lastPart) {
+      this.firstPart = firstPart;
+      this.lastPart = lastPart;
+    }   
+
+    render(container) {
+      const element = document.createElement('div');
+      element.textContent = `${this.firstPart} ${this.lastPart}`;
+      container.appendChild(element);
+    }
+  }
+  
+  // Handling creation and rendering of header lines
+  const lineOne = new headerInfo("name: ", "Christian Belarde");
+  const lineTwo = new headerInfo("role: ", "Web Developer");
+  const lineThree = new headerInfo("location: ", "Seattle, WA");
+  const lineFour = new headerInfo("experience: ", "Over 2 years");
+  const lineFive = new headerInfo("resume: ", "Here");
+  lineOne.render(headerContainer);
+  lineTwo.render(headerContainer);
+  lineThree.render(headerContainer);
+  lineFour.render(headerContainer);
+  lineFive.render(headerContainer);
+
+  const myGreeting = "Hello there!"
+  main.appendChild(myGreeting);
+  const myBio = `My name is Christian. I am 26 yeard old, and a big fan of tech. 
+  I specialize in front-end development, IT Help Desk work, and general promgramming.
+   My true passion is writing code to solve problems and build cool things.`
+}
