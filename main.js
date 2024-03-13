03dd90842dfe319970ffd5e5f3fdbcc2f9b7ec32
Christian Belarde <cjbelarde253@gmail.com>
2024-03-12 22:07:35 -0700

    add logic for work card component creation

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload = function() {
   const main = document.querySelector('#main');
   const headerContainer = document.createElement('div');
-  main.appendChild(headerContainer);
+  const workContainer = document.querySelector('#workContianer');
+  //main.appendChild(headerContainer);
 
   class headerInfo {
     constructor (firstPart, lastPart) {
@@ -15,7 +16,7 @@ window.onload = function() {
       container.appendChild(element);
     }
   }
-
+  /*
   // Handling creation and rendering of header lines
   const lineOne = new headerInfo("name: ", "Christian Belarde");
   const lineTwo = new headerInfo("role: ", "Web Developer");
@@ -33,14 +34,36 @@ window.onload = function() {
   const myBio = `My name is Christian. I am 26 yeard old, and a big fan of tech. 
   I specialize in front-end development, IT Help Desk work, and general promgramming.
    My true passion is writing code to solve problems and build cool things.`
-
+*/
 
   class workCard {
-    constructor(name, logo) {
-      this.name = name;
-      this.logo = logo;
+    constructor(title, url) {
+      this.title = title;
+      this.url = url;
+    }
+    createWorkCard() {
+      //Method to create HTML structure
+      const workContainer = document.getElementById('workContainer');
+      const workCard = document.createElement('div');
+      workCard.classList.add('workCard');
+
+      //Create image and title elements and set their content 
+      const image = document.createElement('img');
+      image.src = this.url;
+      const title = document.createElement('h3');
+      title.textContent = this.title;
+
+      //Append the content to the card
+      workCard.appendChild(image);
+      workCard.appendChild(title);
+
+      workContainer.appendChild(workCard);
     }
   }
+new workCard("DSP Enhancement Solutions", "Assets/dsp-solutions.svg").createWorkCard();
+new workCard("Atera PowerShell Utility", "Assets/atera.svg").createWorkCard();
+new workCard("Adopt-a-buddy", "Assets/adopt.svg").createWorkCard();
+new workCard("Code snippets", "Assets/snippets.svg").createWorkCard();
 
   class workCardExpanded {
     constructor(title, languages, image, bio) {
